220f4201af6e515be96dbbbd5cad63128d95a2b2
Nayana Davis <nayana487@gmail.com>
2015-07-31 13:19:50 -0400

    added stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
   root to: 'posts#index'
   resources :posts do
     resources :comments
@@ -10,6 +9,7 @@ Rails.application.routes.draw do
   post '/sign_up', to: 'users#sign_up!'
   get '/sign_out', to: 'users#sign_out'
 end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -64,4 +64,3 @@ end
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150731155029) do
+ActiveRecord::Schema.define(version: 20150731165945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,11 @@ ActiveRecord::Schema.define(version: 20150731155029) do
     t.string "name"
     t.string "content"
     t.string "title"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
   end
 
   add_foreign_key "comments", "posts"
