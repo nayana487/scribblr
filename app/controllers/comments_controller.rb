f3359ba091faa13c21849009deb1cc4cb43c5942
Nayana Davis <nayana487@gmail.com>
2015-07-31 12:43:36 -0400

    updated files

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,27 @@
+class CommentsController < ApplicationController
+
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
+  end
+
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = Comment.create!(comment_params.merge(post: @post))
+    redirect_to post_path(@post)
+  end
+
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+  def edit
+  @comment = Comment.find(params[:id])
+  end
+
+
+  def  comment_params
+  params.require(:comment).permit(:name, :content, :post_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+
+  root to: 'posts#index'
+  resources :posts do
+    resources :comments
+ end
+  get '/sign_in', to: 'users#sign_in'
+  post '/sign_in', to: 'users#sign_in!'
+  get '/sign_up', to: 'users#sign_up'
+  post '/sign_up', to: 'users#sign_up!'
+  get '/sign_out', to: 'users#sign_out'
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
